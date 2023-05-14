94b2df337bf65a8a0f9f318e12271c98da862a15
Hwan616 <hoan0616@naver.com>
2023-05-15 01:09:39 +0900

    시작 화면 세팅

--- brickout.js
+++ brickout.js
@@ -1,28 +1,29 @@
 $(document).ready(function(){
-    var level;
-    $("#topLevel").on("click",function(){
-        level = 3;
 
-        var Screen = $("#startScreen");
-        Screen.addClass("offScreen");
-        var Game = $("#main_game");
-        Game.removeClass("offScreen");
-    });
-    $("#middleLevel").on("click",function(){
-        level = 2;
-        var Screen = $("#startScreen");
-        Screen.addClass("offScreen");
-        var Game = $("#main_game");
-        Game.removeClass("offScreen");
-    });    
-    $("#lowLevel").on("click",function(){
-        level = 1;
-        var Screen = $("#startScreen");
-        Screen.addClass("offScreen");
-        var Game = $("#main_game");
-        Game.removeClass("offScreen");
-    });
-    var bossHp=100*level; // 보스체력
+    var level = 2;
+    var bossHp = 100*level; // 보스체력
     var velocity = level; // 벽돌 떨어지는 속도
-    
+
+    $("#startButton").on("click", function () {
+        $("#startScreen").addClass("offScreen");
+        $("#main_game").removeClass("offScreen");
+    })
+
+    $("#levelButton").on("click", function () {
+        var levelText = $(this).text();
+        if (levelText == "난이도 : 하") {
+            level = 2;
+            $(this).text("난이도 : 중");
+        }
+        if (levelText == "난이도 : 중") {
+            level = 3;
+            $(this).text("난이도 : 상");
+        }
+        if (levelText == "난이도 : 상") {
+            level = 1;
+            $(this).text("난이도 : 하");
+        }
+        bossHp = 100*level;
+        velocity = level;
+    });
 })

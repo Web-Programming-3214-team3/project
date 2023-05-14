6ad2a7eae7a26249fa84afc2eaac05e2b56a26b2
JunKyu Choi <jkchoi.dev@gmail.com>
2023-05-15 01:13:01 +0900

    feat : keyGame

--- brickout.js
+++ brickout.js
@@ -21,8 +21,23 @@ $(document).ready(function(){
         Screen.addClass("offScreen");
         var Game = $("#main_game");
         Game.removeClass("offScreen");
+        keyGame();
     });
     var bossHp=100*level; // 보스체력
     var velocity = level; // 벽돌 떨어지는 속도
     
-})
+});
+
+function keyGame(){
+    
+    $("#key_game").removeClass("offScreen");
+    var arrows = ["key_down.png", "key_up.png","key_left.png","key_right.png"];
+    var steps = [0,0,0,0,0];
+    for(var i = 0; i<5;i++){
+        steps[i] = Math.floor(Math.random()*4);
+    }
+    var keyImg = $(".key_img").get();
+    for(var i = 0; i<5; i++){
+        keyImg[i].src = arrows[steps[i]];
+    }
+}

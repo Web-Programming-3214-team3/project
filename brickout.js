f5d927de2e5cc91c30dceffba7d5802526d3c248
JunKyu Choi <122720798+junkyn@users.noreply.github.com>
2023-05-14 15:22:21 +0900

    feat : Start Setting

--- brickout.js
+++ brickout.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-    
+    var level;
+    $("#topLevel").on("click",function(){
+        level = 3;
+        var Screen = $("#startScreen");
+        Screen.addClass("offScreen");
+        var Game = $("#main_game");
+        Game.removeClass("offScreen");
+    });
+    $("#middleLevel").on("click",function(){
+        level = 2;
+        var Screen = $("#startScreen");
+        Screen.addClass("offScreen");
+        var Game = $("#main_game");
+        Game.removeClass("offScreen");
+    });    
+    $("#lowLevel").on("click",function(){
+        level = 1;
+        var Screen = $("#startScreen");
+        Screen.addClass("offScreen");
+        var Game = $("#main_game");
+        Game.removeClass("offScreen");
+    });
 })

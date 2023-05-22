bba02cf2e2f55b1df3ef6bb61a6ec1b9c92d5573
JunKyu Choi <jkchoi.dev@gmail.com>
2023-05-22 22:21:29 +0900

    feat : keyGame에서 다른 키 입력 시 시간 바 줄어들게 적용

--- brickout.js
+++ brickout.js
@@ -8,6 +8,7 @@ var BGM = 5;                // 배경음악 크기
 var skip = false;           // 스토리 스킵 여부
 
 var keyGameCount = 5;       // Key Game 반복 횟수
+var keyGametimer = 100; // Key Game 남은 시간
 
 var mole_catch=0;           // 두더지 잡은 횟수
 var mole_miss=0;            // 두더지 놓친 횟수
@@ -190,24 +191,41 @@ $(document).ready(function(){
             i++;
         }
         console.log(keys[i].getElementsByTagName("img")[0].getAttribute("value"));
-        if(keys[i].getElementsByTagName("img")[0].getAttribute("value")=="0"&& e.keyCode=="40"){
-            keys[i].style.display="none";
-            console.log(keys[i]);
+        if(keys[i].getElementsByTagName("img")[0].getAttribute("value")=="0"){
+            if(e.keyCode=="40"){
+                keys[i].style.display="none";
+                console.log(keys[i]);
+            }
+            else{
+                keyGametimer -= 5;
+            }
         }
-        else if(keys[i].getElementsByTagName("img")[0].getAttribute("value")=="1"&& e.keyCode=="38"){
-            keys[i].style.display="none";
-            console.log(keys[i]);
-        
+        else if(keys[i].getElementsByTagName("img")[0].getAttribute("value")=="1"){
+            if(e.keyCode=="38"){
+                keys[i].style.display="none";
+                console.log(keys[i]);
+            }
+            else{
+                keyGametimer -= 5;
+            }
         }
-        else if(keys[i].getElementsByTagName("img")[0].getAttribute("value")=="2"&& e.keyCode=="37"){
-            keys[i].style.display="none";
-            console.log(keys[i]);
-        
+        else if(keys[i].getElementsByTagName("img")[0].getAttribute("value")=="2"){
+            if(e.keyCode=="37"){
+                keys[i].style.display="none";
+                console.log(keys[i]);
+            }
+            else{
+                keyGametimer -= 5;
+            }
         }
-        else if(keys[i].getElementsByTagName("img")[0].getAttribute("value")=="3"&& e.keyCode=="39"){
-            keys[i].style.display="none";
-            console.log(keys[i]);
-        
+        else if(keys[i].getElementsByTagName("img")[0].getAttribute("value")=="3"){
+            if(e.keyCode=="39"){
+                keys[i].style.display="none";
+                console.log(keys[i]);
+            }
+            else{
+                keyGametimer -= 5;
+            }
         }
     });
 
@@ -483,6 +501,7 @@ function keyGame(){
     $("#key_game").removeClass("offScreen");
     var arrows = ["key_down.png", "key_up.png","key_left.png","key_right.png"];
     var steps = [0,0,0,0,0];
+    keyGametimer = 100;
     for(var i = 0; i<5;i++){
         steps[i] = Math.floor(Math.random()*4);
     }
@@ -491,19 +510,17 @@ function keyGame(){
         keyImg[i].src = arrows[steps[i]];
         keyImg[i].setAttribute("value",steps[i]);
     }
-    var timeleft = 10/level;
-    var timer = 100;
+
     var downloadTimer = setInterval(function(){
         $("#key_game_textField").find("input").focus();
         if($(".input_keys").get()[4].style.display=="none"){
             ClearKeyGame();
         }
-        if(timer<=0){
+        if(keyGametimer<=0){
             FailKeyGame();
         }
-        $(".count_Mainbar").css("width",timer+"%");
-        timeleft -=0.05;
-        timer -= (level/10);
+        $(".count_Mainbar").css("width",keyGametimer+"%");
+        keyGametimer -= (level/10);
     },5);
     function ClearKeyGame(){
         clearInterval(downloadTimer);
